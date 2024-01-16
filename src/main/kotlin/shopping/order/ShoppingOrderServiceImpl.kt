719373904850a968fe7a49ad872ccdcf89db2518
Andrey Somov <public.somov@gmail.com>
2024-01-16 15:35:27 +0400

    Refactor argument name to avoid back ticks

--- src/main/kotlin/shopping/order/ShoppingOrderServiceImpl.kt
+++ src/main/kotlin/shopping/order/ShoppingOrderServiceImpl.kt
@@ -12,12 +12,12 @@ import java.util.concurrent.CompletionStage
 class ShoppingOrderServiceImpl : ShoppingOrderService {
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
 
-    override fun order(`in`: OrderRequest): CompletionStage<OrderResponse> {
+    override fun order(input: OrderRequest): CompletionStage<OrderResponse> {
         var total = 0
-        for (item in `in`.itemsList) {
+        for (item in input.itemsList) {
             total += item.quantity
         }
-        logger.info("Order {} items from cart {}.", total, `in`.cartId)
+        logger.info("Order {} items from cart {}.", total, input.cartId)
         val response = OrderResponse.newBuilder().setOk(true).build()
         return CompletableFuture.completedFuture(response)
     }
